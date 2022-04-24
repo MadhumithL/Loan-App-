8460f521a4ff13ea3850c3ac661f9fc0acdca495
MadhumithL <104270578+MadhumithL@users.noreply.github.com>
2022-04-24 10:57:43 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"net/http/httputil"
+	"os"
+)
+
+func getAppRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/createloan", AddContext(CreateLoan)).Methods("POST")
+	router.HandleFunc("/admin/approve/{loanId}", AddContext(ApproveLaon)).Methods("PUT")
+	router.HandleFunc("/track/{loanId}", AddContext(TrackLoanApplication)).Methods("GET")
+	router.HandleFunc("/pay/{loanId}", AddContext(RepayAmount)).Methods("POST")
+	return router
+}
+
+var host = "localhost"
+var port = "8080"
+
+func startServer(r *mux.Router) {
+	go func() {
+		err := http.ListenAndServe("localhost:8080", r)
+		if err != nil {
+			fmt.Println("Starting server failed for %v,%v, with Error : %v", host, port, err)
+			os.Exit(1)
+		}
+	}()
+	return
+}
+
+func main() {
+	ar := getAppRouter()
+	startServer(ar)
+	select {}
+}
+
+func AddContext(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		reqDump, err := httputil.DumpRequest(req, true)
+		if err != nil {
+			fmt.Println("Request failed")
+		}
+		fmt.Println("Request Received----", string(reqDump))
+		next.ServeHTTP(res, req)
+		fmt.Println("Response sent %s,%s", req.Method, req.URL.String())
+	})
+}
